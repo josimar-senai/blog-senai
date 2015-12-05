a1c674923427246b7d79668561fd24b82b64cc5a
jackson_gutz <jackson_gutz@10.1.104.27>
2015-12-05 11:24:07 -0200

    alteração marcos

--- include/menu.php
+++ include/menu.php
@@ -3,8 +3,8 @@
     <li><a href="##">Calendário</a></li>
     <li><a href="./class/Contato.php">Mensagem</a></li>
     <li><a href="?pagina=noticias">Notícias</a></li>
-    <li><a href="##">Tempo</a></li>
-    <li><a href="#pagina/previsãotempo#">Downloads</a></li>
+    <li><a href="pagina/previsãotempo">Tempo</a></li>
+    <li><a href="">Downloads</a></li>
     <li><a href="##">Mapas</a></li>
     <li><a href="##">Tecnologias</a></li>
 </ul>
