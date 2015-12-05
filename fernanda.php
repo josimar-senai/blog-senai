ee9b0dd969054bc68bb2a63197fff1a9c8b45e1a
fernanda_cr <fernanda_cr@10.1.105.16>
2015-12-05 09:54:53 -0200

    Atualizações Fernanda

--- fernanda.php
+++ fernanda.php
@@ -4,7 +4,7 @@
         <title>Blog-Senai</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
     </head>
