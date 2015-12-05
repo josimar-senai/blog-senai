35dd35357c6ad7a06923cdb8d6002141bebebcd5
fernanda_cr <fernanda_cr@10.1.105.16>
2015-12-05 09:50:16 -0200

    Atualizações Fernanda

--- fernanda.php
+++ fernanda.php
@@ -1,8 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Blog-Senai</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+        <div class="container-fluid" style="width: 1024px;">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand active" href="##">Blog-Senai</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li><a href="##">##</a></li>
+                            <li><a href="##">##</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+    </body>
+</html>
