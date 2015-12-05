86bd27a034379336ef69e590f424a35dcb57b40c
josimar_souza1 <josimar_souza1@10.1.105.14>
2015-12-05 11:11:32 -0200

    aletrado o link do menu de novo

--- include/menu.php
+++ include/menu.php
@@ -3,8 +3,8 @@
     <li><a href="?pagina=calendario">Calendário</a></li>
     <li><a href="?pagina=contato">Mensagem</a></li>
     <li><a href="?pagina=noticias">Notícias</a></li>
-    <li><a href="?pagina=previsãotempo">Tempo</a></li>
-    <li><a href="?pagina==download">Downloads</a></li>
+    <li><a href="?pagina=previsaotempo">Tempo</a></li>
+    <li><a href="?pagina=download">Downloads</a></li>
     <li><a href="?pagina=mapas">Mapas</a></li>
     <li><a href="?pagina=tecnologias">Tecnologias</a></li>
 </ul>
