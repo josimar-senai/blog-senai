7a194c3594b22da6a20645668cc6a0abaed546c8
fernanda_cr <fernanda_cr@10.1.105.16>
2015-12-05 09:52:48 -0200

    Atualizações Fernanda

---
+++ bootstrap-3.2.0-dist/js/ie-emulation-modes-warning.js
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br" xml:lang="pt-br">
+<head>
+<title>Objeto n&atilde;o encontrado!</title>
+<link rev="made" href="mailto:postmaster@localhost" />
+<style type="text/css"><!--/*--><![CDATA[/*><!--*/ 
+    body { color: #000000; background-color: #FFFFFF; }
+    a:link { color: #0000CC; }
+    p, address {margin-left: 3em;}
+    span {font-size: smaller;}
+/*]]>*/--></style>
+</head>
+
+<body>
+<h1>Objeto n&atilde;o encontrado!</h1>
+<p>
+
+
+    A URL requisitada n&atilde;o foi encontrada neste servidor.
+
+  
+
+    O link na
+    <a href="http://127.0.0.1/tcc/index.php">p&aacute;gina
+    referida</a> parece estar com algum erro ou desatualizado. Por favor informe o
+    autor <a href="http://127.0.0.1/tcc/index.php">desta 
+    p&aacute;gina</a> sobre o erro.
+
+   
+
+</p>
+<p>
+Se voc&ecirc; acredita ter encontrado um problema no servidor,
+por favor entre em contato com o 
+<a href="mailto:postmaster@localhost">webmaster</a>.
+
+</p>
+
+<h2>Error 404</h2>
+<address>
+  <a href="/">127.0.0.1</a><br />
+  <span>Apache/2.4.2 (Win32) OpenSSL/1.0.1c PHP/5.4.4</span>
+</address>
+</body>
+</html>
+
