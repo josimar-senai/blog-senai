7f8d7f2bbdea62e9e1fff4b8854b0f60867f8586
marcos_jose_soares <marcos_jose_soares@10.1.105.69>
2015-12-05 11:38:44 -0200

    Merge origin/master

---
+++ include/menu.php
@@ -0,0 +1,10 @@
+<ul class="nav nav-tabs">
+    <li><a href="##">Home</a></li>
+    <li><a href="##">Calendário</a></li>
+    <li><a href="##">Mensagem</a></li>
+    <li><a href="##">Notícias</a></li>
+    <li><a href="##">Tempo</a></li>
+    <li><a href="##">Downloads</a></li>
+    <li><a href="##">Mapas</a></li>
+    <li><a href="##">Tecnologias</a></li>
+</ul>
--- index.php
+++ index.php
@@ -1,3 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Blog-Senai</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
 
-echo "teste 12";
+        <div class="container-fluid" style="width: 1024px;">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand active" href="index.php">Blog-Senai</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <?php
+                        require './include/menu.php';
+                        if (isset($_GET['pagina'])) {
+                            require "./pagina/{$_GET['pagina']}.php";
+                        }
+                        ?>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </body>
+</html>
