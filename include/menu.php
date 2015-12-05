077158c24fde09193e768ede47af0d47433edde6
marcos_jose_soares <marcos_jose_soares@10.1.105.69>
2015-12-05 12:05:45 -0200

    alteração marcos

--- include/menu.php
+++ include/menu.php
@@ -2,8 +2,8 @@
     <li><a href="##">Home</a></li>
     <li><a href="##">Calendário</a></li>
     <li><a href="./class/Contato.php">Mensagem</a></li>
-    <li><a href="?pagina=noticias">Notícias</a></li>
-    <li><a href="pagina/previsãotempo">Tempo</a></li>
+    <li><a href="./pagina/noticias.php">Notícias</a></li>
+    <li><a href="./pagina/previsaotempo.php">Tempo</a></li>
     <li><a href="">Downloads</a></li>
     <li><a href="##">Mapas</a></li>
     <li><a href="##">Tecnologias</a></li>
