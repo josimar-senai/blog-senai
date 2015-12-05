54d23610f33199f66441d9fa0f5d3086e39cb2ee
fernanda_cr <fernanda_cr@10.1.105.16>
2015-12-05 11:29:14 -0200

    contato

--- include/menu.php
+++ include/menu.php
@@ -1,7 +1,7 @@
 <ul class="nav nav-tabs">
     <li><a href="##">Home</a></li>
     <li><a href="##">Calendário</a></li>
-    <li><a href="./class/Contato.php">Mensagem</a></li>
+    <li><a href="./pagina/Contato.php">Mensagem</a></li>
     <li><a href="./pagina/noticias.php">Notícias</a></li>
     <li><a href="./pagina/previsaotempo.php">Tempo</a></li>
     <li><a href="">Downloads</a></li>
