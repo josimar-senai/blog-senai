e45c570456e0836cdf5198a0369843a1fcc25656
fernanda_cr <fernanda_cr@10.1.105.16>
2015-12-05 10:40:16 -0200

    Atualizações Fernanda

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
         <title>Blog-Senai</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
 
