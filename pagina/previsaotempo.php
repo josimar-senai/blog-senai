7268d6215746e0a4a03b3708ae4a7430420c55af
marcos_jose_soares <marcos_jose_soares@10.1.105.69>
2015-12-07 21:26:23 -0200

    atualização marcos

--- pagina/previsaotempo.php
+++ pagina/previsaotempo.php
@@ -6,14 +6,21 @@
 </div>
 
 
-<div class="pull-left">
-    <p class="lead blog-description ">Tempo Agora</p>
-    <iframe frameborder="0" allowtransparency="yes" scrolling="no" width="300" height="200" src="http://www.tempoagora.com.br/ta-widgets?cidades=,RiodoSul-SC&amp;tipo=horizontal"></iframe>
-</div>
-<div></div>
-<div class="pull-left">
-    <p class="lead blog-description ">CPTEC</p>
-    <iframe allowtransparency="true" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" src="http://www.cptec.inpe.br/widget/widget.php?p=4388&w=h&c=767676&f=ffffff" height="200px" width="215px"></iframe>
-    <noscript>Previs&atilde;o de <a href="http://www.cptec.inpe.br/cidades/tempo/4388">Rio do Sul/SC</a> oferecido por <a href="http://www.cptec.inpe.br">CPTEC/INPE</a>
-    </noscript><!-- Widget Previs&atilde;o de Tempo CPTEC/INPE -->
+<div class="row ">
+    <div class="col-md-4 ">
+        <p class="lead blog-description ">Tempo Agora</p>
+        <iframe frameborder="0" allowtransparency="yes" scrolling="no" width="300" height="200" src="http://www.tempoagora.com.br/ta-widgets?cidades=,RiodoSul-SC&amp;tipo=horizontal"></iframe>
+    </div>
+
+    <div class=" col-md-3">
+        <p class="lead blog-descriptio"> CPTEC</p>
+        <iframe allowtransparency="true" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" src="http://www.cptec.inpe.br/widget/widget.php?p=4388&w=h&c=767676&f=ffffff" width="300" height="300"></iframe>
+        <noscript>Previs&atilde;o de <a href="http://www.cptec.inpe.br/cidades/tempo/4388">Rio do Sul/SC</a> oferecido por <a href="http://www.cptec.inpe.br">CPTEC/INPE</a>
+        </noscript>
+    </div>
+    
+   <div class=" col-md-3">
+        <p class="lead blog-description ">ClimaTempo</p>
+        <iframe scrolling="no" frameborder="0" marginwidth="0" marginheight="0" width="300" height="200" src="http://selos.climatempo.com.br/selos/MostraSelo.php?CODCIDADE=792&SKIN=preto"></iframe>
+    </div>
 </div>
