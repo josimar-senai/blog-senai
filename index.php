caac6d3d85c38fd34925093ba057d9a221baab0b
josimar_souza1 <josimar_souza1@10.1.105.14>
2015-12-07 20:00:00 -0200

    alterações feitas para se trabalhar com POO, para configurar as pagina visualize o arquivo de teste dentro da pasta -> pagina arquivo Home2.php

--- index.php
+++ index.php
@@ -1,34 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Blog-Senai</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/jquery-ui.min.css">
-        <link rel="stylesheet" href="css/jquery-ui.theme.min.css">
-        <script src="js/jquery-1.11.3.min.js"></script>
-        <script src="js/jquery-ui.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-    </head>
-    <body>
-
-        <div class="container-fluid" style="width: 1024px;">
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <a class="navbar-brand active" href="index.php">Blog-Senai</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <?php
-                        require './include/menu.php';
-                        if (isset($_GET['pagina'])) {
-                            require "./pagina/{$_GET['pagina']}.php";
-                        }
-                        ?>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </body>
-</html>
+<?php
+if (isset($_GET['pagina'])) {
+    $classname = ucfirst($_GET['pagina']);
+    require "./class/Pagina.php";
+    require "./pagina/{$classname}.php";
+    try {
+        if(!class_exists($classname, false)){
+            throw new Exception('O arquivo ' . $_GET['pagina'] . '.php' . ' Não é uma subclasse que herda da superclasse Pagina!');
+        }
+        new $classname();
+    } catch (Exception $exc) {
+        echo 'não foi possivel inicial a classe: ' . $classname, '<br>';
+        echo $exc->getMessage();
+    }
+}else{
+    header('location:index.php?pagina=home', null, 302);
+    exit;
+}
