f563e47b92b1ca7ce3f73c48cfa36143ecfc29cd
marcos_jose_soares <marcos_jose_soares@10.1.105.69>
2015-12-07 21:42:19 -0200

    atualização marcos

--- pagina/contato.php
+++ pagina/contato.php
@@ -1,3 +1,26 @@
+<?php
+
+/**
+ * classe responsavel por mostrar o conteudo da pagina
+ *
+ * @author josimar_souza1
+ */
+
+//estanciar a superclasse pagina
+class contato extends Pagina {
+    //metodo abstrato obrigatorio
+    //aki vai as configurações da pagina
+    protected function configura() {
+        //seta o titulo da pagina que aparece na aba do navegador
+        $this->setTitulo("Página inicial do site");
+    }
+    
+    //metodo abstrato obrigatorio
+    //aki fica o conteudo da pagina
+    protected function conteudo() {
+        //o HTML puro que vai aparecer no corpo do documento
+        ?>
+        
 <form class="form-horizontal">
     <fieldset>
         <!-- Form Name -->
@@ -40,3 +63,8 @@
 
     </fieldset>
 </form>
+
+        <?php
+    }
+
+}
