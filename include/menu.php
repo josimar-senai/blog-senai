be48529e8ca53bc132e0abed4abe477e5b62fee0
lucas_fabricio1 <lucas_fabricio1@10.1.105.8>
2015-12-07 20:11:30 -0200

    Atualização do Lucas

--- include/menu.php
+++ include/menu.php
@@ -5,6 +5,6 @@
     <li><a href="?pagina=noticias">Notícias</a></li>
     <li><a href="?pagina=previsaotempo">Tempo</a></li>
     <li><a href="?pagina=download">Downloads</a></li>
-    <li><a href="?pagina=mapas">Mapas</a></li>
+    <li><a href="?pagina=mapa">Mapas</a></li>
     <li><a href="?pagina=tecnologias">Tecnologias</a></li>
 </ul>
